bc061504a3dd0c85252fbfdcb0512735af4412f1
Patriks <puzulispatriks@gmail.com>
2025-10-20 20:52:06 +0300

    PHP work needs to be done

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/home.css">
 
     <!-- Title font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -36,12 +36,24 @@
 <body>
     <box class="box">
         <navbar class="nav">
-
+            <a href="profile.php" class="profile"><img class="prof_img" src="img/user.png" alt=""></a>
+            <div class="searchbar">
+                <form class="" action="" method="post">
+                    <input class="" type="text" placeholder="Search your songs..." name="search">
+                    <select name="Filter" class="dropdown" name="dropdown">
+                        <option value="" disabled selected>Filter</option>
+                        <option value="A">Title A->Z</option>
+                        <option value="Z">Title Z->A</option>
+                        <option value="new">From newest</option>
+                        <option value="old">From oldest</option>
+                    </select>
+                    <input type="submit" class="submit" name="submit">
+                </form>
+            </div>
+            <form action=" " method="post">
+                <button type="submit" name="logout">Logout</button>
+            </form>
         </navbar>
-        <form action=" " method="post">
-            <button type="submit" name="logout">Logout</button>
-        </form>
-        <a href="profile.php">Profile</a>
         <a href="write.php">Write</a>        
     </box>
 </body>
