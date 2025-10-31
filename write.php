ca559781fb61b4eef6e246e56b32341bead07c86
Patriks <puzulispatriks@gmail.com>
2025-10-31 12:42:02 +0200

    Finding chord API is impossible!

--- write.php
+++ write.php
@@ -6,6 +6,10 @@
         header('location: index.php');
         exit();
     }
+
+    $chord = "C";
+    $json = file_get_contents('https://api.uberchord.com/v1/chords/Bb');
+    $data = json_decode($json, true);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +19,16 @@
     <title>Write</title>
 </head>
 <body>
-    <navbar></navbar>
+    <box class="box">
+        <navbar class="nav">
+            <a href="home.php" class="home"><img src="img/binder.png" alt="binder - home"></a>
+            
+        </navbar>
+        <search class="chords">
+            <?php
+                echo $data;
+            ?>
+        </search>
+    </box>
 </body>
 </html>
